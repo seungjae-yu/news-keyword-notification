c2763b2d15c6288861a64c66a23726ac19aa5191
seungjae-yu <kimug2145@gmail.com>
2022-04-12 11:58:13 +0900

    port change

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5001);
 
 app.get('/test', (req: Request, res: Response, next: NextFunction) => {
     res.send('hello test');
+});
+
+app.get('/naverApi', (req: Request, res: Response) => {
+    console.log(req.body);
+    //https://openapi.naver.com/api/v1/search/news.json
 });
