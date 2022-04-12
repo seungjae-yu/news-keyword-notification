31380f54e330632ee51462b2df2f0f71e4f968a5
seungjae-yu <kimug2145@gmail.com>
2022-04-12 11:31:57 +0900

    modify default port in back-end

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,11 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
+app.listen(process.env.PORT || 5000);
 
 app.get('/test', (req: Request, res: Response, next: NextFunction) => {
     res.send('hello test');
-});
-
-app.listen('3000', () => {
-
 });
